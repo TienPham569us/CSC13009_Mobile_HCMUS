5b82dfe2c306652b7304604c842271e430431bd5
21120569 <21120569@student.hcmus.edu.vn>
2023-11-22 23:29:37 +0700

    fix download

--- app/src/main/java/com/example/imagesgallery/Activity/MainActivity.java
+++ app/src/main/java/com/example/imagesgallery/Activity/MainActivity.java
@@ -23,10 +23,14 @@ import android.content.pm.Signature;
 import android.content.res.Configuration;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.ParcelFileDescriptor;
 import android.os.Parcelable;
 import android.provider.MediaStore;
@@ -62,18 +66,22 @@ import com.google.android.material.navigation.NavigationBarView;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-
+import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     public static SQLiteDatabase db;
@@ -337,6 +345,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private Dialog downloadDialog;
     Button btnDownloadImage;
     private void showDialogNavBottom() {
         dialogNavBottom = new Dialog(MainActivity.this);
@@ -349,36 +358,62 @@ public class MainActivity extends AppCompatActivity {
         btnDownloadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressDialog = new ProgressDialog(MainActivity.this);
+                /*progressDialog = new ProgressDialog(MainActivity.this);
                 progressDialog.setMessage("Loading ...");
                 progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                 progressDialog.show();
 
-                String imageUrl="https://en.wikipedia.org/wiki/Main_Page#/media/File:Australiformis_Distribution.png";
-                downloadImage(imageUrl);
+                //https://en.wikipedia.org/wiki/Wikipedia:In_the_news/Candidates#/media/File:Ryan_blaney_(52866797550)_(cropped).jpg
+                String imageUrl="https://en.wikipedia.org/";//https://en.wikipedia.org/wiki/Main_Page#/media/File:Australiformis_Distribution.png";
+                //downloadImage(imageUrl);
+                String imageUrl2 = "https://en.wikipedia.org/wiki/Main_Page#/media/File:Australiformis_Distribution.png";
+                //(imageUrl2);
+                downLoadImage3(imageUrl2);*/
+                showDownloadButton();
+                dialogNavBottom.dismiss();
+
             }
         });
-        /*btnAdd = (Button) dialogNavBottom.findViewById(R.id.buttonAdd);
-        btnCancel = (Button) dialogNavBottom.findViewById(R.id.buttonCancel);
-
-
-        // when click button add of dialog
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "add",Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-        // when click button cancel of dialog
-
-
-        dialogNavBottom.show();
+       dialogNavBottom.show();
 
     }
 
+    private EditText edtImageUrl;
+    private Button btnStartDownload;
+    private Button btnCancelDownload;
+    private void showDownloadButton() {
+        downloadDialog = new Dialog(MainActivity.this);
+        downloadDialog.setContentView(R.layout.download_image_dialog);
+        downloadDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        downloadDialog.getWindow().setGravity(Gravity.CENTER);
+
+
+        edtImageUrl = (EditText) downloadDialog.findViewById(R.id.edtImageUrl);
+        btnStartDownload = (Button) downloadDialog.findViewById(R.id.buttonDownloadImage);
+        btnCancelDownload = (Button) downloadDialog.findViewById(R.id.buttonCancelDownload);
+
+        btnStartDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = edtImageUrl.getText().toString();
+                new FetchImage(url).start();
+            }
+        });
+        btnCancelDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadDialog.dismiss();
+            }
+        });
+
+
+        downloadDialog.show();
+
+    }
+
+
     boolean writeFileToStorage(ResponseBody body) {
-        String nameOfFile = "Downloaded_image"+System.currentTimeMillis()+".jpg";
+        String nameOfFile = "Downloaded_image_group_9_"+System.currentTimeMillis()+".jpg";
 
         File location = null;
 
@@ -392,7 +427,7 @@ public class MainActivity extends AppCompatActivity {
                 ContentValues values = new ContentValues();
                 values.put(MediaStore.Images.Media.TITLE,nameOfFile);
                 values.put(MediaStore.Images.Media.DISPLAY_NAME,nameOfFile);
-                values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg");
+                values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpg");
 
                 Uri uri = null;
                 uri = getContentResolver().insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values);
@@ -406,7 +441,8 @@ public class MainActivity extends AppCompatActivity {
                 long fileSize = body.contentLength();
                 long fileSizeDownloaded = 0;
 
-                OutputStream outputstream = new FileOutputStream(fileDescriptor);
+                //OutputStream outputstream = new FileOutputStream(fileDescriptor);
+                OutputStream outputstream = new ParcelFileDescriptor.AutoCloseOutputStream(descriptor);
                 while (true) {
                     int read = inputStream.read(fileReader);
 
@@ -423,6 +459,14 @@ public class MainActivity extends AppCompatActivity {
 
                 outputstream.flush();
 
+                MediaScannerConnection.scanFile(
+                        this,
+                        new String[]{location.getAbsolutePath()},
+                        new String[]{"image/jpg"},
+                        null
+                );
+
+
                 if (inputStream!=null) {
                     inputStream.close();
                 }
@@ -431,8 +475,8 @@ public class MainActivity extends AppCompatActivity {
                     outputstream.close();
                 }
 
-                File readLocation = new File(Environment.getExternalStoragePublicDirectory(
-                                        Environment.DIRECTORY_DOWNLOADS)+"/"+nameOfFile);
+                //File readLocation = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/"+nameOfFile);
+                File readLocation = new File(location.getAbsolutePath());
                 Log.d(TAG, "Read location: "+readLocation);
                 //setImageDrawable(drawable.createFromPath(readLocation.sotString()));
 
@@ -451,7 +495,7 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit =builder.build();
 
         DownloadService downloadService = retrofit.create(DownloadService.class);
-        Call<ResponseBody> call = downloadService.downloadFileFromUrl(fileUrl);
+        Call<ResponseBody> call = downloadService.downloadFileFromUrl("wiki/Wikipedia:In_the_news/Candidates#/media/File:Ryan_blaney_(52866797550)_(cropped).jpg");
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -459,6 +503,7 @@ public class MainActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     try {
                         boolean writeToDisk = writeFileToStorage(response.body());
+                        Toast.makeText(MainActivity.this,"file downloaded or not status -> "+writeToDisk,Toast.LENGTH_SHORT).show();
                         Log.d(TAG,"file downloaded or not status -> "+writeToDisk);
 
                     } catch (Exception e) {
@@ -467,6 +512,7 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
                     Log.d(TAG,"server connection error");
+                    Toast.makeText(MainActivity.this,"server connection error",Toast.LENGTH_SHORT).show();
                 }
                 progressDialog.dismiss();
             }
@@ -474,9 +520,208 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
                 Log.d(TAG,"Something went wrong");
-                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this,"Something went wrong",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    public void downloadImage2(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+
+            // Get the file name from the URL
+            //tring fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            String fileName  = "Downloaded_image_group_9_"+System.currentTimeMillis()+ imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+            // Create a file in the device's external storage directory
+            File directory = Environment.getExternalStorageDirectory();
+            File file = new File(directory, fileName);
+
+            // Create a FileOutputStream to write the image data to the file
+            FileOutputStream fileOutput = new FileOutputStream(file);
+
+            // Read the image data from the input stream and write it to the file
+            InputStream inputStream = connection.getInputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fileOutput.write(buffer, 0, bytesRead);
+            }
+
+            // Close the streams
+            fileOutput.close();
+            inputStream.close();
+
+            // Optionally, you can scan the downloaded image file with the MediaScanner
+            // to make it available in the device's gallery or media library
+            MediaScannerConnection.scanFile(
+                    this,
+                    new String[]{file.getAbsolutePath()},
+                    null,
+                    null
+            );
+
+            // Optionally, you can show a toast or log a message to indicate the successful download
+            // Toast.makeText(this, "Image downloaded successfully", Toast.LENGTH_SHORT).show();
+            // Log.d("Download", "Image downloaded successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle any errors that occurred during the download process
+        }
+    }
+    public void downLoadImage3(String imageUrl) {
+        OkHttpClient httpClient = new OkHttpClient.Builder().build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://example.com/") // Set the base URL of your API
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        DownloadService imageApiService = retrofit.create(DownloadService.class);
+        Call<ResponseBody> call = imageApiService.downloadFileFromUrl(imageUrl);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    // Save the image to a file
+                    saveImageToFile(response.body());
+                } else {
+                    // Handle unsuccessful response
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // Handle failure
+            }
+        });
+    }
+
+    private void saveImageToFile(ResponseBody responseBody) {
+        try {
+            // Prepare the file to save the image
+            File directory = Environment.getExternalStorageDirectory();
+            File file = new File(directory, "image.jpg");
+
+            // Create an input stream to read the response body
+            InputStream inputStream = responseBody.byteStream();
+
+            // Create an output stream to write the image data to the file
+            OutputStream outputStream = new FileOutputStream(file);
+
+            // Read the data from the input stream and write it to the output stream
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            // Close the streams
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            // Optionally, you can scan the downloaded image file with the MediaScanner
+            // to make it available in the device's gallery or media library
+            MediaScannerConnection.scanFile(
+                    this,
+                    new String[]{file.getAbsolutePath()},
+                    null,
+                    null
+            );
+
+            // Optionally, you can show a toast or log a message to indicate the successful download
+            // Toast.makeText(this, "Image downloaded successfully", Toast.LENGTH_SHORT).show();
+            // Log.d("Download", "Image downloaded successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle any errors that occurred during the file-saving process
+        }
+    }
+    private Handler mainHandler =new Handler();
+    public class FetchImage extends Thread{
+        String url;
+        Bitmap bitmap;
+
+        public FetchImage(String url) {
+            this.url=url;
+        }
+
+        @Override
+        public void run() {
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setMessage("Loading ... ");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+
+                }
+            });
+
+            InputStream inputStream = null;
+            try {
+                inputStream = new URL(this.url).openStream();
+                this.bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            mainHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        //save
+                        String fileName = "download_image_group_9_"+url.substring(url.lastIndexOf("/") + 1);
+                        saveBitmapToExternalStorage(bitmap,fileName);
+                    }
+                }
+            });
+
+        }
+    }
+    private void saveBitmapToExternalStorage(Bitmap bitmap, String fileName) {
+        // Get the external storage directory
+        String externalStorageDirectory = Environment.getExternalStorageDirectory().toString();
+
+        // Create a file object with the desired directory and file name
+        File file = new File(externalStorageDirectory, fileName);
+
+        try {
+            // Create an output stream to write the bitmap data to the file
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            // Compress the bitmap data into the output stream as JPEG with 100% quality (you can adjust the quality as needed)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            // Flush and close the output stream
+            outputStream.flush();
+            outputStream.close();
+
+            // Optionally, you can trigger a media scan to make the saved image visible in the device's gallery or media library
+            MediaScannerConnection.scanFile(
+                    this,
+                    new String[]{file.getAbsolutePath()},
+                    null,
+                    null
+            );
+
+            // Optionally, you can show a toast or log a message to indicate the successful save
+            // Toast.makeText(this, "Bitmap saved successfully", Toast.LENGTH_SHORT).show();
+            // Log.d("SaveBitmap", "Bitmap saved successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle any errors that occurred during the saving process
+        }
     }
 
 }
